f09a743eab9bd5ab7cc836407af484ed51f1208c
Harish Kumar Gangula <harish@sanketika.in>
2024-04-08 10:27:25 +0530

    Issue #000 feat: Basic backend app added

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const helmet = require('helmet')
+const app = express()
+const port = 3000
+
+app.use(helmet())
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
