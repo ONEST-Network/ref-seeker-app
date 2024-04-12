5470cbc7dd965eef8f1cb258c1d69c907e7b568d
Vinod Kumar <vinodkumar@zool.in>
2024-04-12 13:45:10 +0530

    color variables created

--- seeker-portal/src/App.js
+++ seeker-portal/src/App.js
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import './App.css';
+import './App.scss';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
