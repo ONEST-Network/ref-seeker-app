c6f888935d8c2e516a8fb64e03e8ef054bc4860b
Vinod Kumar <vinodkumar@zool.in>
2024-04-12 12:43:20 +0530

    search field added

--- seeker-portal/src/App.js
+++ seeker-portal/src/App.js
@@ -9,7 +9,10 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import './App.css';
 import Grid from '@mui/material/Grid';
-
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
   return (
@@ -50,131 +53,139 @@ export default function BasicTabs() {
   };
 
   return (
+    <>
+    <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center'}} className='search-box'>
+      <IconButton type="button" sx={{ p: '10px' }} aria-label="search" color='primary'>
+        <SearchIcon />
+      </IconButton>
+      <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search" inputProps={{ 'aria-label': 'search' }} />
+    </Paper>
+    
     <Box sx={{ width: '100%' }}>
-      <Box>
-        <Tabs value={value} onChange={handleChange} aria-label="tabs" className='tabs-list'>
-          <Tab label="Jobs" {...a11yProps(0)} />
-          <Tab label="Scholarships" {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0} className="tabs-panel">
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 12, sm: 8, md: 12 }}>
-          <Grid item xs={12} sm={4} md={4}>
-            <Card sx={{ display: 'flex' }} className='tabs-card'>
-              <img src={logo} className="card-image" alt="logo" />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
-                  <Typography component="h2" className='card-title'>
-                    Data Entry Operator
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    UrbanTech
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    Delhi, India (Remote)
-                  </Typography>
-                  <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
-                    Posted By National Career Service
-                    <span className='time'>4hrs ago</span>
-                  </Typography>
-                </CardContent>
-              </Box>
-            </Card>
+        <Box>
+          <Tabs value={value} onChange={handleChange} aria-label="tabs" className='tabs-list'>
+            <Tab label="Jobs" {...a11yProps(0)} />
+            <Tab label="Scholarships" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0} className="tabs-panel">
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 12, sm: 8, md: 12 }}>
+            <Grid item xs={12} sm={4} md={4}>
+              <Card sx={{ display: 'flex' }} className='tabs-card'>
+                <img src={logo} className="card-image" alt="logo" />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
+                    <Typography component="h2" className='card-title'>
+                      Data Entry Operator
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      UrbanTech
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      Delhi, India (Remote)
+                    </Typography>
+                    <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
+                      Posted By National Career Service
+                      <span className='time'>4hrs ago</span>
+                    </Typography>
+                  </CardContent>
+                </Box>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={4} md={4}>
+              <Card sx={{ display: 'flex' }} className='tabs-card'>
+                <img src={logo} className="card-image" alt="logo" />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
+                    <Typography component="h2" className='card-title'>
+                      Data Entry Operator
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      UrbanTech
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      Delhi, India (Remote)
+                    </Typography>
+                    <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
+                      Posted By National Career Service
+                      <span className='time'>8hrs ago</span>
+                    </Typography>
+                  </CardContent>
+                </Box>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={4} md={4}>
+              <Card sx={{ display: 'flex' }} className='tabs-card'>
+                <img src={logo} className="card-image" alt="logo" />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
+                    <Typography component="h2" className='card-title'>
+                      Data Entry Operator
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      UrbanTech
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      Delhi, India (Remote)
+                    </Typography>
+                    <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
+                      Posted By National Career Service
+                      <span className='time'>12hrs ago</span>
+                    </Typography>
+                  </CardContent>
+                </Box>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={4} md={4}>
+              <Card sx={{ display: 'flex' }} className='tabs-card'>
+                <img src={logo} className="card-image" alt="logo" />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
+                    <Typography component="h2" className='card-title'>
+                      Data Entry Operator
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      UrbanTech
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      Delhi, India (Remote)
+                    </Typography>
+                    <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
+                      Posted By National Career Service
+                      <span className='time'>2hrs ago</span>
+                    </Typography>
+                  </CardContent>
+                </Box>
+              </Card>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={4} md={4}>
-          <Card sx={{ display: 'flex' }} className='tabs-card'>
-              <img src={logo} className="card-image" alt="logo" />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
-                  <Typography component="h2" className='card-title'>
-                    Data Entry Operator
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    UrbanTech
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    Delhi, India (Remote)
-                  </Typography>
-                  <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
-                    Posted By National Career Service
-                    <span className='time'>8hrs ago</span>
-                  </Typography>
-                </CardContent>
-              </Box>
-            </Card>
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1} className="tabs-panel">
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 12, sm: 8, md: 12 }}>
+            <Grid item xs={12} sm={4} md={4}>
+              <Card sx={{ display: 'flex' }} className='tabs-card'>
+                <img src={logo} className="card-image" alt="logo" />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
+                    <Typography component="h2" className='card-title'>
+                      Data Entry Operator
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      UrbanTech
+                    </Typography>
+                    <Typography component="h4" className='card-sub-title'>
+                      Delhi, India (Remote)
+                    </Typography>
+                    <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
+                      Posted By National Career Service
+                      <span className='time'>6hrs ago</span>
+                    </Typography>
+                  </CardContent>
+                </Box>
+              </Card>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={4} md={4}>
-          <Card sx={{ display: 'flex' }} className='tabs-card'>
-              <img src={logo} className="card-image" alt="logo" />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
-                  <Typography component="h2" className='card-title'>
-                    Data Entry Operator
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    UrbanTech
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    Delhi, India (Remote)
-                  </Typography>
-                  <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
-                    Posted By National Career Service
-                    <span className='time'>12hrs ago</span>
-                  </Typography>
-                </CardContent>
-              </Box>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={4} md={4}>
-          <Card sx={{ display: 'flex' }} className='tabs-card'>
-              <img src={logo} className="card-image" alt="logo" />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
-                  <Typography component="h2" className='card-title'>
-                    Data Entry Operator
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    UrbanTech
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    Delhi, India (Remote)
-                  </Typography>
-                  <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
-                    Posted By National Career Service
-                    <span className='time'>2hrs ago</span>
-                  </Typography>
-                </CardContent>
-              </Box>
-            </Card>
-          </Grid>
-        </Grid>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1} className="tabs-panel">
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 12, sm: 8, md: 12 }}>
-          <Grid item xs={12} sm={4} md={4}>
-          <Card sx={{ display: 'flex' }} className='tabs-card'>
-              <img src={logo} className="card-image" alt="logo" />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }} className='tabs-card-contet'>
-                  <Typography component="h2" className='card-title'>
-                    Data Entry Operator
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    UrbanTech
-                  </Typography>
-                  <Typography component="h4" className='card-sub-title'>
-                    Delhi, India (Remote)
-                  </Typography>
-                  <Typography sx={{ display: 'flex', justifyContent: 'space-between' }} variant="subtitle1" component="div" className='card-sub-title2'>
-                    Posted By National Career Service
-                    <span className='time'>6hrs ago</span>
-                  </Typography>
-                </CardContent>
-              </Box>
-            </Card>
-          </Grid>
-          </Grid>
-      </CustomTabPanel>
-    </Box>
+        </CustomTabPanel>
+      </Box></>
   );
 }
