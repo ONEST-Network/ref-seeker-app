c3e912560bbe8e48564400a844d7277b82209900
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2024-04-17 11:56:52 +0530

    Issue #12 feat: Created React App with Typescript

--- seeker-portal/src/App.tsx
+++ seeker-portal/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import BasicTabs from './components/dashboard/Dashboard'
-
+import logo from './logo.svg';
 import './App.css';
-import Grid from '@mui/material/Grid';
 
 function App() {
   return (
     <div className="App">
-  <BasicTabs/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- seeker-portal/src/index.tsx
+++ seeker-portal/src/index.tsx
@@ -3,20 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
+  <React.StrictMode>
     <App />
-  </ThemeProvider>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
