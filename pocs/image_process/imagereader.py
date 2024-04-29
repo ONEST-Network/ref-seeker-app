30dae4faa941e4303b400d0321b80eb33af9e086
bharghava k <bharghava.k@tibilsolutions.com>
2024-04-29 14:55:53 +0530

    portrait mode

--- pocs/image_process/imagereader.py
+++ pocs/image_process/imagereader.py
@@ -1,12 +1,41 @@
 import easyocr
-reader = easyocr.Reader(['en','en']) # this needs to run only once to load the model into memory
-result = reader.readtext(r'C:\Users\DELL\Downloads\vott\pan.jpg')
+import cv2
 
-data = [item[1] for item in result]  # Extracting the third element from each tuple
-doc_type = "pan"
+reader = easyocr.Reader(['en','en'])
+
+def rotate_image(image, angel):
+
+    rotation_matrix = cv2.getRotationMatrix2D((image.shape[1] / 2, image.shape[0] / 2), angel, 1)
+
+    rotated_image = cv2.warpAffine(image, rotation_matrix, (image.shape[1], image.shape[0]))
+    return rotated_image
+
+def extract_text_from_image(image):
+    result = reader.readtext(image)
+    data = [item[1] for item in result]
+    return data
+
+def checkDataAndReturn_Text(data, indx, image):
+
+    if len(data[0]) < 3:
+        angles = [90, 180, 270]
+        if indx < len(angles):
+            imager = cv2.imread(image)
+            if imager is None:
+                raise ValueError(f"Cannot read the image from {image}")
+
+            rotated_image = rotate_image(imager, angles[indx])
+            data1 = extract_text_from_image(rotated_image)
+            return checkDataAndReturn_Text(data1, indx + 1, image)
+        else:
+            return data
+    return data
+
+image_path = r'C:\Users\DELL\Pictures\Screenshots\Screenshot 2024-04-25 133607.png'
+text = extract_text_from_image(image_path)
+data = checkDataAndReturn_Text(text, 0, image_path)
+doc_type = "aadhar"
 da = {}
-
-# print(data)
 
 if doc_type == "aadhar" :
     da['name'] = data[3]
@@ -24,6 +53,5 @@ elif doc_type == 'voter':
     da['father_name'] = data[13]
     da['gender'] = data[16].split(' ')[1]
     da['DOB']  = data[17].split(':')[1]
-
 
 print (doc_type, da)
